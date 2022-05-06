df4c158f2a333bdac6d0885d309ea86480e670d5
Dainius Balsys <dainytis@gmail.com>
2022-05-06 21:37:12 +0300

    Adding annotations

--- src/main/java/dainius/app/blog/repository/entity/Post.java
+++ src/main/java/dainius/app/blog/repository/entity/Post.java
@@ -1,16 +1,34 @@
 package dainius.app.blog.repository.entity;
 
 import java.time.LocalDateTime;
-import lombok.Data;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
-@Data
+@Entity
+@NoArgsConstructor
+@Getter
 public class Post {
 
-  private final int id;
-  private final String header;
-  private final String text;
-  private final LocalDateTime dateAndTime;
-  private final int userId;
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private int id;
+
+  @Column (name = "header")
+  private String header;
+
+  @Column (name = "text")
+  private String text;
+
+  @Column (name = "date_time")
+  private LocalDateTime dateAndTime;
+
+  @Column (name = "user_id")
+  private int userId;
 
 }
 
