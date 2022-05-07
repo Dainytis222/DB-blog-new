559b878ecc8ff11049d525c88f597247f396f295
Dainius Balsys <dainytis@gmail.com>
2022-05-07 17:10:12 +0300

    Testing Pageable method

--- src/main/java/dainius/app/blog/repository/PostRepository.java
+++ src/main/java/dainius/app/blog/repository/PostRepository.java
@@ -1,8 +1,14 @@
 package dainius.app.blog.repository;
 
 import dainius.app.blog.repository.entity.Post;
+import java.util.List;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface PostRepository extends JpaRepository<Post, Integer> {
 
+//  List<Post> findPostsByUserId (int user);
+  Page<Post> findAllById(int id, Pageable pageable);
 }
